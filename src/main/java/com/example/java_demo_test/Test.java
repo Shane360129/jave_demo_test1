1944c033ce3dd7a6e60efcd3b914779b3c786b98
DESKTOP-L10ENU7\a0920 <a0920360129@gmail.com>
2023-04-10 11:16:00 +0800

    Second

---
+++ src/main/java/com/example/java_demo_test/Test.java
@@ -0,0 +1,9 @@
+package com.example.java_demo_test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a*b);
+    }
+}
