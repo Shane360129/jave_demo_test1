68a9a32440559b37c1b054ce93d4af60edeb01bd
DESKTOP-L10ENU7\a0920 <a0920360129@gmail.com>
2023-04-11 11:47:35 +0800

    0411

---
+++ src/main/java/com/example/java_demo_test/Bank/Bank.java
@@ -0,0 +1,22 @@
+package com.example.java_demo_test.Bank;
+
+public class Bank {
+    private int ID;
+    private int account;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
+}
